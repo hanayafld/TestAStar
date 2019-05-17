69b358aa0cca67360b67238dba7985937df637d1
hanayafld <hanayafld@gmail.com>
2019-05-17 17:49:15 +0900

    20190517-pm0547
    원점으로 부터 한번 버튼누르면
    자기 주변 8칸(파랑블럭 제외) 노란블럭으로 색 변경까지 함
    
    다음 : 화살표 추가하기, 밖에서 설정할 수 있게 하드코딩 풀기

---
+++ Assets/Scripts/TestAStar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestAStar : MonoBehaviour
+{
+    public Tile[] Tiles;
+    public Button btn;
+
+    private Tile[,] arrTiles = new Tile[5, 7];
+    private Tile startTile;
+
+    private List<Tile> openList = new List<Tile>();
+    private List<Tile> closeList = new List<Tile>();
+
+    void Start()
+    {
+        //이차원 배열만들기
+        int k = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                this.arrTiles[i, j] = Tiles[k];
+                Debug.Log(arrTiles[i, j].pos);
+                k++;
+            }
+        }
+
+        this.startTile = arrTiles[2, 1];
+
+        this.btn.onClick.AddListener(() =>
+        {
+            Debug.Log("버튼누름");
+            this.startTile.imageGo.GetComponent<SpriteRenderer>().color = Color.red;
+            int x = (int)this.arrTiles[2, 1].pos.x;
+            int y = (int)this.arrTiles[2, 1].pos.y;
+            
+            var pos = this.arrTiles[x, y].pos;
+            
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    int newX = (int)pos.x + i;
+                    int newY = (int)pos.y + j;
+                    
+                    if (newX == pos.x && newY == pos.y)
+                    {
+                        this.closeList.Add(this.arrTiles[newX, newY]);
+                    }
+                    else if(arrTiles[newX,newY].tileType!=1)
+                    {
+                        this.openList.Add(this.arrTiles[newX, newY]);
+                        foreach (var data in openList)
+                        {
+                            data.imageGo.GetComponent<SpriteRenderer>().color = Color.yellow;
+                        }
+                    }
+                }
+            }
+
+        });
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public TextMesh textMeshF;
+    public TextMesh textMeshG;
+    public TextMesh textMeshH;
+    public TextMesh textMeshPos;
+    public GameObject imageGo;
+
+    //int 0 : normal // 1 : impos // 2 : start // 3 : end 
+    public int tileType;
+
+    public int f;
+    public int g;
+    public int h;
+
+    public Vector2 pos;
+
+    void Start()
+    {
+        StartCoroutine(this.SetText());
+        if (this.tileType == 1)
+        {
+            this.imageGo.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+        else if (this.tileType == 2)
+        {
+            this.imageGo.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+        else if (this.tileType == 3)
+        {
+            this.imageGo.GetComponent<SpriteRenderer>().color = Color.gray;
+        }
+        else
+        {
+            this.imageGo.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+    }
+
+    private IEnumerator SetText()
+    {
+        while (true)
+        {
+            this.pos = new Vector2(-this.transform.position.y, this.transform.position.x);
+            this.textMeshPos.text = string.Format("{0}, {1}", -this.transform.position.y, this.transform.position.x);
+            //this.textMeshF.text = this.f.ToString();
+            //this.textMeshG.text = this.g.ToString();
+            //this.textMeshH.text = this.h.ToString();
+
+            yield return null;
+        }
+    }
+}
